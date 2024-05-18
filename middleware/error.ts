80aff2ef37412af41fe42d92760735df388946aa
Igbalajobi Zhikrullah <zalajobi@gmail.com>
2024-05-18 17:47:21 +0100

    update error middleware

--- middleware/error.ts
+++ middleware/error.ts
@@ -23,80 +23,80 @@ export const errorMiddleware = async (
   // Schema Validation Error
   if (err instanceof ZodError) {
     console.log('Schema Validation Error');
-    JsonApiResponse(res, 'Schema Validation Error', false, {
+    JsonApiResponse(res, 'Schema Validation Error', false, null, 400, {
       error: {
         type: 'validation_error',
         message: 'Validation error',
         errors: err.format(),
         name: err.name
       },
-    }, 400);
+    });
     return;
   }
 
   // Query Failed Error
   if (err instanceof QueryFailedError) {
     console.log('Database Query Failed');
-    JsonApiResponse(res, 'Query Failed Error', false, {
+    JsonApiResponse(res, 'Query Failed Error', false, null, 500, {
       error: {
         type: 'database_error',
         message: err.message,
         errors: err.stack,
         name: err.name,
       },
-    }, 500);
+    });
     return;
   }
 
   // Entity Not Found
   if (err instanceof EntityNotFoundError) {
     console.log('Entity Not Found');
-    JsonApiResponse(res, 'Entity Not Found', false, {
+    JsonApiResponse(res, 'Entity Not Found', false, null, 404, {
       error: {
         type: 'database_error',
         message: err.message,
         name: err.name,
       },
-    }, 404);
+    });
     return;
   }
 
   // Query Runner Already Released Error
   if (err instanceof QueryRunnerAlreadyReleasedError) {
     console.log('Query Runner Already Released');
-    JsonApiResponse(res, 'Query Runner Already Released', false, {
+    JsonApiResponse(res, 'Query Runner Already Released', false, null, 500, {
       error: {
         type: 'database_error',
         message: err.message,
         name: err.name,
       },
-    }, 500);
+    });
     return
   }
 
   // Transaction Already Started Error
   if (err instanceof TransactionAlreadyStartedError) {
     console.log('Transaction Already Started');
-    JsonApiResponse(res, 'Transaction Already Started', false, {
+    JsonApiResponse(res, 'Transaction Already Started', false, null, 500, {
       error: {
         type: 'database_error',
         message: 'Transaction already started',
         name: err.name,
       },
-    }, 500);
+    });
     return;
   }
 
   // Transaction Not Started Error
   if (err instanceof TransactionNotStartedError) {
     console.log('Transaction Not Started');
-    JsonApiResponse(res, 'Transaction Not Started', false, {
+    JsonApiResponse(res, 'Transaction Not Started', false, null, 500, {
       error: {
         type: 'database_error',
         message: 'Transaction not started',
         name: err.name,
       },
-    }, 500);
+    });
     return;
   }
 
@@ -105,13 +105,13 @@ export const errorMiddleware = async (
     console.log('JWT Error');
     if (err instanceof UpdateValuesMissingError) {
       console.log('Missing Update Body');
-      JsonApiResponse(res, 'Missing Update Body', false, {
+      JsonApiResponse(res, 'Missing Update Body', false, null, 401, {
         error: {
           type: 'jwt_error',
           message: err.message,
           name: err.name,
         },
-      }, 401);
+      });
     }
     return;
   }
@@ -119,65 +119,63 @@ export const errorMiddleware = async (
   // Error Updating Data - Missing Columns to update
   if (err instanceof UpdateValuesMissingError) {
     console.log('Missing Update Body');
-    JsonApiResponse(res, 'Missing Update Body', false, {
+    JsonApiResponse(res, 'Missing Update Body', false, null, 500, {
       error: {
         type: 'postgres_error',
         message: err.message,
         name: err.name,
       },
-    }, 500);
+    });
     return;
   }
 
   // Entity Not Found Error - TypeORM Error
   if (err instanceof EntityPropertyNotFoundError) {
     console.log('Entity Property Not Found');
-    JsonApiResponse(res, 'Entity Property Not Found', false, {
+    JsonApiResponse(res, 'Entity Property Not Found', false, null, 500, {
       error: {
         type: 'postgres_error',
         message: err.message,
         name: err.name,
       },
-    }, 500);
+    });
     return;
   }
 
   // Badly Formed JSON Error From Post request
   if (err instanceof SyntaxError && 'body' in err) {
     console.log('Bad JSON');
-    JsonApiResponse(res, 'Bad JSON', false, {
+    JsonApiResponse(res, 'Bad JSON', false, null, 400, {
       error: {
         type: 'bad_json',
         message: err.message,
         name: err.name,
       }
-    }, 400);
+    });
     return;
   }
 
   // JWT TOKEN Expired Error
   if (err.name === 'TokenExpiredError') {
     console.log('JWT Token Expired');
-    JsonApiResponse(res, 'Token Expired', false, {
+    JsonApiResponse(res, 'Token Expired', false, null, 401, {
       error: {
         type: 'jwt_error',
         message: 'Token Expired',
         name: err.name,
       }
-    }, 401);
+    });
     return;
   }
 
   // Generic Error
   if (err instanceof Error) {
     console.log('General Error');
-    JsonApiResponse(res, 'Token Expired', false, {
-      error: {
-        type: 'api_error',
+    JsonApiResponse(res, 'Error', false, null, 500, {
+      type: 'api_error',
         message: err.message,
         name: err.name,
-      }
-    }, 500);
+    });
     return;
   }
 };
