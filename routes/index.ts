672a798a313c6bfadfb7c5bb5fa892be5dccfb4a
Igbalajobi Zhikrullah <zalajobi@gmail.com>
2024-05-18 19:45:00 +0100

    create task hanler

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import {BASE_URL} from "@lib/config";
 import userRouter from "@routes/userRoutes";
+import taskRouter from "@routes/taskRoute";
 
 const rootRouter = Router();
 
 rootRouter.use(`${BASE_URL}/user`, userRouter);
+rootRouter.use(`${BASE_URL}/task`, taskRouter);
 
 export default rootRouter;
