9257ad33e9da132f6e4eca5d7500c3f4713f6e2c
Igbalajobi Zhikrullah <zalajobi@gmail.com>
2024-05-18 17:05:07 +0100

    create redis - refresh-token storage

--- lib/config.ts
+++ lib/config.ts
@@ -18,3 +18,9 @@ export const PASSWORD_HASH_SECRET = process.env.PASSWORD_HASH_SECRET!;
 export const JWT_ACCESS_TOKEN = process.env.JWT_ACCESS_TOKEN!;
 
 export const JWT_REFRESH_TOKEN = process.env.JWT_REFRESH_TOKEN!;
+
+export const REDIS_HOST = process.env.REDIS_HOST!;
+
+export const REDIS_PORT = process.env.REDIS_PORT!;
+
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD!;
